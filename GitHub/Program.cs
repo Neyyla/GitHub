cffd94f3ce9c742f025dc57e610f3ab06906de3d
neyla <neyla@LAPTOP-M0EMNMOB>
2022-01-18 20:17:52 +0100

    Optimisation du code en n'écrivant qu'une fois la création des 2 nombres alatoires.

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -5,8 +5,47 @@ namespace GitHub
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            int choix = 0;
+            int val1 = 0;
+            int val2 = 0;
+            int reponse;
+            int solution;
+        
+            
+
+
+            if (choix != 0)
+
+                if (choix == 1)
+                {
+                    val1 = Random.Next(1, 10);
+                    val2 = Random.Next(1, 10);
+
+                    Console.WriteLine(val1 + " + " + val2 + " = ");
+                    reponse = int.Parse(Console.ReadLine());
+
+                    solution = val1 + val2;
+                    if (reponse == solution)
+                    {
+                        Console.WriteLine("Bravo !");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Faux : " + val1 + " + ");
+                    }
+
+                }
+                else
+                {
+                    val1 = Random.Next(1, 10);
+                    val2 = Random.Next(1, 10);
+
+                    Console.Write(val1 + " x " + val2 + " = ");
+                    reponse = int.Parse(Console.ReadLine());
+                }
+                
+        
+    
+
